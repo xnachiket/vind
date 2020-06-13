78bff09b1990a64bd85b36f8519e00370b85ec5e
xnachiket <55916117+xnachiket@users.noreply.github.com>
2020-06-13 23:04:04 +0530

    Add files via upload

---
+++ background.js
@@ -0,0 +1,13 @@
+chrome.commands.onCommand.addListener(command => {
+  if (command === 'ntab') {
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, ([tab]) => {
+      if (!tab) return;
+      chrome.tabs.sendMessage(tab.id, 'getText', text => {
+        if(text != ''){
+        chrome.tabs.create({
+          url: 'https://www.google.com/search?q=' + encodeURIComponent(text),
+        });
+      } });
+    });
+  }
+});
---
+++ content.js
@@ -0,0 +1,6 @@
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  if (msg === 'getText' && document.hasFocus()
+  && (!document.activeElement || !/^i?frame$/.test(document.activeElement.localName))) {
+    sendResponse(getSelection().toString());
+  }
+});
